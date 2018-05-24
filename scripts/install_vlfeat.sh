52b9b83a1aadeda2de493718535b64f3b79f7e9f
Yu-Chen Xue <32350820+SeanSyue@users.noreply.github.com>
2018-05-24 19:06:51 +0800

    Update install_vlfeat.sh
    update vlfeat to v0.9.21

--- scripts/install_vlfeat.sh
+++ scripts/install_vlfeat.sh
@@ -2,21 +2,21 @@
 echo "downloading and installing precompiled VLFeat binaries..."
 cd ../
 mkdir -p bin/opt/download
-mkdir -p bin/opt/vlfeat-0.9.20
+mkdir -p bin/opt/vlfeat-0.9.21
 mkdir -p bin/vlfeat
 rm -rf bin/vlfeat
-rm -rf bin/opt/vlfeat-0.9.20
+rm -rf bin/opt/vlfeat-0.9.21
 cd bin/opt/download
-wget -N http://www.vlfeat.org/download/vlfeat-0.9.20-bin.tar.gz || exit 1
+wget -N http://www.vlfeat.org/download/vlfeat-0.9.21-bin.tar.gz || exit 1
 sum=$(md5sum vlfeat-0.9.20-bin.tar.gz)
 vlsum="e22ada7ddd708d739ed9958b16642ba1"
 if [[ $sum = *$vlsum* ]]
   then
     echo "md5 sum was valid ($sum)"
     echo "extracting archive..."
-    tar -xzvf vlfeat-0.9.20-bin.tar.gz || exit 1
-    cp vlfeat-0.9.20/bin/glnxa64/libvl.so ../../
-    mv vlfeat-0.9.20 ../ || exit 1
+    tar -xzvf vlfeat-0.9.21-bin.tar.gz || exit 1
+    cp vlfeat-0.9.21/bin/glnxa64/libvl.so ../../
+    mv vlfeat-0.9.21 ../ || exit 1
     echo "done"
   else
     echo "bad checksum: $sum  expected: $vlsum"
